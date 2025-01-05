25642a17182107fea0dd9c7b08762ae6911f1606
Aliou DIALLO <aliou.diallo4@unchk.edu.sn>
2025-01-05 23:21:07 +0000

    Add info and image to page experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -9,6 +9,9 @@ import imgTYPE from "../assets/techno/typescript.svg";
 import imgNODE from "../assets/techno/node-js.png";
 import imgPRISMA from "../assets/techno/prisma.webp";
 import imgNEXT from "../assets/techno/next-js.webp";
+import imgLARAVEL from "../assets/techno/laravel.png";
+import imgPHP from "../assets/techno/php.png";
+import imgMySQL from "../assets/techno/mysql.png";
 
 import google from "../assets/companies/google.png";
 import amazon from "../assets/companies/amazon.png";
@@ -23,7 +26,10 @@ const skills = [
     {id: 6, name: "Tailwind CSS", image: imgTAILWIND},
     {id: 7, name: "TypeScript", image: imgTYPE},
     {id: 8, name: "Next.js", image: imgNEXT},
-    {id: 9, name: "Prisma", image: imgPRISMA},
+    {id: 9, name: "PHP", image: imgPHP},
+    {id: 10, name: "Laravel", image: imgLARAVEL},
+    {id: 11, name: "MySQL", image: imgMySQL},
+    {id: 12, name: "Prisma", image: imgPRISMA},
 ];
 
 
